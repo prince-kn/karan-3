ae6ea3eab1216ee2d099f8c046592477ab930da8
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-06-14 07:05:55 +0545

    Update help2.js

--- modules/commands/help2.js
+++ modules/commands/help2.js
@@ -73,7 +73,7 @@ module.exports. run = function({ api, event, args, getText }) {
     for (let item of returnArray) msg += `✰『 ${++i} 』 ➬${item} \n`;
     
     
-    const siu = `Page Cmds  💯💯💖𝐌𝐚𝐝𝐞 𝐁𝐲 𝐀𝐫𝐮𝐧 𝐤𝐮𝐦𝐚𝐫`;
+    const siu = `Page Cmds  MADE BY RKO BRO👑💝`;
     
  const text = `\nPage (${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)})`;
  
