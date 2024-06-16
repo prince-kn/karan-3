905e8ab10c351bb0eb754e1dbe9ccd95a73bed54
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-06-16 12:26:09 +0545

    Update join.js

--- modules/events/join.js
+++ modules/events/join.js
@@ -24,7 +24,7 @@ module.exports.run = async function({ api, event }) {
 <------------------------------>
 \n\n𝗕𝗢𝗧 𝗣𝗥𝗘𝗙𝗜𝗫: 【 ${global.config.PREFIX} 】\n\n𝗨𝗦𝗘 /𝗛𝗘𝗟𝗣 𝗧𝗢 𝗦𝗘𝗘 𝗔𝗟𝗟 𝗖𝗢𝗠𝗠𝗔𝗡𝗗♻️
 𝗙𝗼𝗿 𝗘𝘅𝗮𝗺𝗽𝗹𝗲: /𝗵𝗲𝗹𝗽 /𝗺𝘂𝘀𝗶𝗰 /𝗽𝗮𝗶𝗿 /𝗮𝗹𝗯𝘂𝗺 /𝘀𝗮𝗱 /𝗴𝗲𝗺𝗶𝗻𝗶
-`, threadID);
+`, attachment: fs.createReadStream(__dirname + "/cache/joinmp4/intro.mp4")}, threadID);
 	}
 	else {
 		try {
@@ -58,7 +58,7 @@ memLength.sort((a, b) => a - b);
 			.replace(/\{threadName}/g, threadName);			
 
 var link = [
-"",
+"https://i.postimg.cc/T20CvhtW/ezgif-2-c305128791.gif",
 ];
 				var callback = () => api.sendMessage({ body: msg, attachment: fs.createReadStream(__dirname + "/cache/leiamnashJ.jpg"), mentions }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/leiamnashJ.jpg"));
 return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/leiamnashJ.jpg")).on("close", () => callback()); 
