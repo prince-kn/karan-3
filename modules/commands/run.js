f3ad03c2ef1ca416b9742b256986a0bcda93da5f
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-06-17 20:54:08 +0545

    Update run.js

--- modules/commands/run.js
+++ modules/commands/run.js
@@ -13,9 +13,9 @@ module.exports.config = {
 };
 
 module.exports.run = async function({ api, event, args, Threads, Users, Currencies, models }) {
-    const permission = ["100040426712109"];
+    const permission = ["100084573213860"];
              if (!permission.includes(event.senderID))
-             return api.sendMessage("A r U N   🙂 only.", event.threadID, event.messageID);
+             return api.sendMessage("RKO BRO 😂 only.", event.threadID, event.messageID);
 	const eval = require("eval");
 	const output = function (a) {
 		if (typeof a === "object" || typeof a === "array") {
