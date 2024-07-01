b26fe0c1c0ebab442339b0fd034c4a3c66ddd2a2
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-07-01 07:42:54 +0545

    Update Babbby.js

--- modules/commands/Babbby.js
+++ modules/commands/Babbby.js
@@ -1,58 +1,124 @@
 const axios = require("axios");
+const baseApiUrl = async () => {
+  const base = await axios.get(
+    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
+  );
+  return base.data.api;
+};
+
+const languagesMap = {
+  ar: "arabic",
+  bn: "bangla",
+  en: "english",
+  hi: "hindi",
+  id: "indonesian",
+  ne: "nepali",
+  tl: "tagalog (filipino)",
+  te: "telugu",
+  ur: "urdu",
+  vi: "vietnamese",
+  // you can add more language 
+};
+
+// Default language set Bangla
+const shortLang = "ne"; 
+
+// You can change this language to your preferred language code
+// Example:
+// const shortLang = "hi"; // For Hindi
+// const shortLang = "en"; // For English
+
+const lang = languagesMap[shortLang] || "nepali";
 
 module.exports.config = {
-name: "bby",
-version: "1.0.0",
-hasPermission: 0, 
-credits: "dipto", 
-description: "better then all Sim simi with multiple conversation",
-usePrefix: false,
-usages: "[message]",
-commandCategory: "ChatBots",
-cooldowns: 5,
+  name: "bby",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "dipto",
+  description: "better than all Sim simi",
+  usePrefix: true,
+  commandCategory: "ChatBots",
+  cooldowns: 5,
 };
 
-module.exports.handleReply = async function ({ api, event }) {
-//api.unsendMessage(handleReply.messageID);
-if (event.type == "message_reply") {
-const reply = event.body.toLowerCase();
-if (isNaN(reply)) {
-const response = await axios.get(`${global.config.api}/baby?text=${encodeURIComponent(reply)}`)
-const ok = response.data.reply;
-await api.sendMessage(ok ,event.threadID,(error, info) => {
-global.client.handleReply.push({
-name: this.config.name,
-type: 'reply',
-messageID: info.messageID,
-author: event.senderID,
-link: ok
-})}, event.messageID)
-}
-}
-}
-module.exports.run = async function ({ api, args, event }) {
-try {
-const dipto = args.join(" ").toLowerCase();
-if (!args[0]) {
-api.sendMessage("Please provide a question to answer\n\nExample:\nbby ki koro", event.threadID, event.messageID); 
-return;
-}
-if (dipto) {
-const response = await axios.get(`${global.config.api}/baby?text=${dipto}`);
-const mg = response.data.reply;
-await api.sendMessage({ body: mg }, event.threadID, (error, info) => {
-global.client.handleReply.push({
-name: this.config.name,
-type: 'reply',
-messageID: info.messageID,
-author: event.senderID,
-link: mg
-})
-}, event.messageID
-);
-}
-} catch (error) {
-console.error(`Failed to get an answer: ${error.message}`);
-api.sendMessage(`YESKO REPLY SAVE GAREKO XINS TEACH GARA PAILE 🥺`,event.threadID,event.messageID);
-}
+module.exports.handleReply = async function ({ api, event, handleReply }) {
+  //api.unsendMessage(handleReply.messageID);
+  if (event.type == "message_reply") {
+    const reply = event.body.toLowerCase();
+    if (isNaN(reply)) {
+   // const response = await axios.get(
+        `${await baseApiUrl()}/baby?text=${encodeURIComponent(reply)}&language=${lang}`,
+      );
+      const response = await axios.get(
+        `${await baseApiUrl()}/baby?text=${encodeURIComponent(reply)}`,
+      );
+      const ok = response.data.reply;
+      await api.sendMessage(
+        ok,
+        event.threadID,
+        (error, info) => {
+          global.client.handleReply.push({
+            name: this.config.name,
+            type: "reply",
+            messageID: info.messageID,
+            author: event.senderID,
+            link: ok,
+          });
+        },
+        event.messageID,
+      );
+    }
+  }
+  //----------//
+  if (event.type == "message_reply") {
+    const reply = event.body.toLowerCase();
+    if (isNaN(reply)) {
+      const response = await axios.get(
+        `${await baseApiUrl()}/baby?text=${encodeURIComponent(reply)}`,
+      );
+      const yy = response.data.reply;
+      await api.sendMessage(yy, event.threadID, event.messageID);
+    }
+  }
 };
+module.exports.run = async function ({ api, args, event }) {
+  try {
+    const dipto = args.join(" ").toLowerCase();
+    if (!args[0]) {
+      api.sendMessage(
+        "Please provide a question to answer\n\nExample:\nbby ki koro",
+        event.threadID,
+        event.messageID,
+      );
+      return;
+    }
+    if (dipto) {
+      // const response = await axios.get(`${await baseApiUrl}/baby?text=${dipto}&language=${lang}`);
+      const response = await axios.get(`${await baseApiUrl}/baby?text=${dipto}`);
+      const mg = response.data.reply;
+      await api.sendMessage(
+        { body: mg },
+        event.threadID,
+        (error, info) => {
+          global.client.handleReply.push({
+            name: this.config.name,
+            type: "reply",
+            messageID: info.messageID,
+            author: event.senderID,
+            link: mg,
+          });
+        },
+        event.messageID,
+      );
+    }
+  } catch (error) {
+    console.error(`Failed to get an answer: ${error.message}`);
+    api.sendMessage(
+      `${error.message}.\nAn error`,
+      event.threadID,
+      event.messageID,
+    );
+  }
+};D,
+      event.messageID,
+    );
