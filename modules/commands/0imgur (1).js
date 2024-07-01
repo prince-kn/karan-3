ceef0dede31dc3c36fd236d37ecea4e562e3a5ab
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-07-01 22:17:16 +0545

    Update 0imgur (1).js

--- modules/commands/0imgur (1).js
+++ modules/commands/0imgur (1).js
@@ -18,7 +18,7 @@ module.exports.run = async function ({ api, event }) {
       return api.sendMessage('Please reply to an image or video.', event.threadID, event.messageID);
     }
     try {
-      const res = await axios.get(`https://nobs-api.onrender.com/dipto/imgur?url=${encodeURIComponent(dip)}`);
+      const res = await axios.get(`https://www.noobs-api.000.pe/dipto/imgur?url=${encodeURIComponent(dip)}`);
       const dipto = res.data.data;
          api.sendMessage(dipto, event.threadID, event.messageID);
     } catch (error) {
