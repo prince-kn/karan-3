a0fd2c5ee71e7cbd834938cc42961069c08fa766
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-07-01 13:33:35 +0545

    Update Babbby.js

--- modules/commands/Babbby.js
+++ modules/commands/Babbby.js
@@ -19,7 +19,7 @@ const reply = event.body.toLowerCase();
 if (isNaN(reply)) {
 const response = await axios.get(`${global.config.api}/baby?text=${encodeURIComponent(reply)}`)
 const ok = response.data.reply;
-await api.sendMessage(ok ,event.threadID,(error, info) => {
+await api.sendMessage(ok ,event.threadID,( info) => {
 global.client.handleReply.push({
 name: this.config.name,
 type: 'reply',
@@ -34,13 +34,13 @@ module.exports.run = async function ({ api, args, event }) {
 try {
 const dipto = args.join(" ").toLowerCase();
 if (!args[0]) {
-api.sendMessage("Please provide a question to answer\n\nExample:\nbby k gardai xau ", event.threadID, event.messageID); 
+api.sendMessage("Please provide a question to answer\n\nExample:\nbby k gardai xau", event.threadID, event.messageID); 
 return;
 }
 if (dipto) {
 const response = await axios.get(`${global.config.api}/baby?text=${dipto}`);
 const mg = response.data.reply;
-await api.sendMessage({ body: YESKO REPLY SAVE GAREKO XINS TEACH GARA PAILE 🥺 }, event.threadID, (error, info) => {
+await api.sendMessage({ body: mg }, event.threadID, (error, info) => {
 global.client.handleReply.push({
 name: this.config.name,
 type: 'reply',
