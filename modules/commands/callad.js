ebf0e31cbd46e874ef536df5750a3d556e6c839f
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-07-05 00:02:32 +0545

    Update callad.js

--- modules/commands/callad.js
+++ modules/commands/callad.js
@@ -1,7 +1,7 @@
 module.exports.config = {
   name: "callad",
   version: "1.0.0",
-  hasPermssion: 0,
+  hasPermssion: 2,
   credits: "NTKhang update & fix by DuyVuong and D-jukie Mod by TuấnDz",
   description: "thông báo lỗi của bot đến admin hoặc góp ý",
   commandCategory: "Admin",
