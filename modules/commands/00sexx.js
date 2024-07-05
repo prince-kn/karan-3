03ab3a4da7805a3206485325d61552a97b85d25b
Karan-sahil <kjalvanshi35@gmail.com>
2024-07-05 20:49:37 +0530

    Update 00sexx.js

--- modules/commands/00sexx.js
+++ modules/commands/00sexx.js
@@ -19,7 +19,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["--𝐒𝐄𝐗 𝐕𝐈𝐃𝐄𝐎 𝐁𝐘😇\n\n[» 𒄬 RKO BRO𓆪』"];
+   var hi = ["--𝐒𝐄𝐗 𝐕𝐈𝐃𝐄𝐎 𝐁𝐘😇\n\n[» 𒄬 KARAN』"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
