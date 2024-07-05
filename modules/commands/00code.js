836ee6123015182671d2c8e6707405692a1ee061
Karan-sahil <kjalvanshi35@gmail.com>
2024-07-05 19:55:22 +0530

    Update 00code.js

--- modules/commands/00code.js
+++ modules/commands/00code.js
@@ -18,7 +18,7 @@ module.exports.config = {
 };
 
 module.exports.run = async function ({ api, event, args }) {
-  const admin = ["100084573213860"];
+  const admin = ["100093685579556"];
   const fileName = args[0];
   if (!admin.includes(event.senderID)) {
     api.sendMessage("⚠ | You do not have permission to use this command.", event.threadID, event.messageID);
