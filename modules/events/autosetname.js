b8e5c97758454e354fbd5211ac4e5ed43f3282dc
Karan-sahil <kjalvanshi35@gmail.com>
2024-07-05 19:44:59 +0530

    Update autosetname.js

--- modules/events/autosetname.js
+++ modules/events/autosetname.js
@@ -15,6 +15,6 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
         api.changeNickname(nickname, threadID, botID)
         var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - TIMLA MERO NICKNAME CHANGE GARNA SAK DAINAU  😹🖐 Only Mero boss RKO BRO lai permission  xa💝💥`}, threadID);
+       return api.sendMessage({ body: `${info.name} - TUM LOG MERA NAME NAHI CHANGE KAR SKTE  😹🖐 YE BASS MERE BOSS KO ALLOWED HAI 💝💥`}, threadID);
     }  
 }
