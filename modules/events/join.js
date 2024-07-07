733ce13313dc637b404c362f82d3d4ae711d60a2
prince-kn <pe64sahani@gmail.com>
2024-07-08 00:45:44 +0530

    Update join.js

--- modules/events/join.js
+++ modules/events/join.js
@@ -54,7 +54,7 @@ memLength.sort((a, b) => a - b);
 			.replace(/\{threadName}/g, threadName);			
 
 var link = [
-"https://i.postimg.cc/T20CvhtW/ezgif-2-c305128791.gif",
+"https://i.imghippo.com/files/j1Vpq1720379677.jpg",
 ];
 				var callback = () => api.sendMessage({ body: msg, attachment: fs.createReadStream(__dirname + "/cache/leiamnashJ.jpg"), mentions }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/leiamnashJ.jpg"));
 return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/leiamnashJ.jpg")).on("close", () => callback()); 
