fa48a4ce8173e6374aa81acd0c06b61f890554b3
prince-kn <pe64sahani@gmail.com>
2024-07-09 05:03:26 +0530

    Update upt2.js

--- modules/commands/upt2.js
+++ modules/commands/upt2.js
@@ -1,27 +1,111 @@
-module.exports.config = {
-  name: "fbmp4",
-  version: "1.0.0",
-  hasPermssion: 0,
-  credits: "HungCho",
-  description: "",
-  commandCategory: "Media",
-  usages: "/fbmp4 [link video] dạng 1000xxxx/video/idviddeo",
-  cooldowns: 10,
-  dependencies: {
-    "request": "",
-    "fs": "",
-    "node-cmd": "",
-    "ytdl-core": "",
-    "fb-tools": ""
-  }
-};
+/code edit uptime const os = require("os");
+const fs = require("fs-extra");
 
-module.exports.run = async({api,event,args}) => {
-  const fs = require("fs");
- const facebookTools = global.nodemodule["fb-tools"];
- const axios = require("axios");
- const name = args.join(" ")
-  videoObj = await facebookTools.getVideoUrl(name);
-  var callback = () => api.sendMessage({body: `Hi ?`,attachment: fs.createReadStream(__dirname + "/src/a2.mp3")}, event.threadID, () => fs.unlinkSync(__dirname + "/src/a2.mp3")); 
-      return request(encodeURI(`${videoObj.sdLink}`)).pipe(fs.createWriteStream(__dirname+"/src/a2.mp3")).on("close",() => callback());
-       };
+const startTime = new Date(); // Moved outside onStart
+
+module.exports = {
+  config: {
+    name: "uptime",
+    version: "1.0.0",
+    hasPermssion: 2,
+    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    description: "test",
+    commandCategory: "box",
+    usages: "test",
+    dependencies: {},
+    cooldowns: 5
+  },
+
+  run: async function ({ api, event, args }) {
+    try {
+      const uptimeInSeconds = (new Date() - startTime) / 1000;
+
+      const seconds = uptimeInSeconds;
+      const days = Math.floor(seconds / (3600 * 24));
+      const hours = Math.floor((seconds % (3600 * 24)) / 3600);
+      const minutes = Math.floor((seconds % 3600) / 60);
+      const secondsLeft = Math.floor(seconds % 60);
+      const uptimeFormatted = `${days}d ${hours}h ${minutes}m ${secondsLeft}s`;
+
+      const loadAverage = os.loadavg();
+      const cpuUsage =
+        os
+          .cpus()
+          .map((cpu) => cpu.times.user)
+          .reduce((acc, curr) => acc + curr) / os.cpus().length;
+
+      const totalMemoryGB = os.totalmem() / 1024 ** 3;
+      const freeMemoryGB = os.freemem() / 1024 ** 3;
+      const usedMemoryGB = totalMemoryGB - freeMemoryGB;
+
+     // const allUsers = await usersData.getAll();
+     // const allThreads = await threadsData.getAll();
+      const currentDate = new Date();
+      const options = { year: "numeric", month: "numeric", day: "numeric" };
+      const date = currentDate.toLocaleDateString("en-US", options);
+      const time = currentDate.toLocaleTimeString("en-US", {
+        timeZone: "Asia/Kolkata",
+        hour12: true,
+      });
+
+      const timeStart = Date.now();
+      await api.sendMessage({
+        body: "🔎| checking........",
+      }, event.threadID);
+
+      const ping = Date.now() - timeStart;
+
+      let pingStatus = "⛔| 𝖡𝖺𝖽 𝖲𝗒𝗌𝗍𝖾𝗆";
+      if (ping < 1000) {
+        pingStatus = "✅| 𝖲𝗆𝗈𝗈𝗍𝗁 𝖲𝗒𝗌𝗍𝖾𝗆";
+      }
+      const systemInfo = `♡   ∩_∩
+ （„• ֊ •„)♡
+╭─∪∪────────────⟡
+│ 𝗨𝗣𝗧𝗜𝗠𝗘 𝗜𝗡𝗙𝗢
+├───────────────⟡
+│ ⏰ 𝗥𝗨𝗡𝗧𝗜𝗠𝗘
+│  ${uptimeFormatted}
+├───────────────⟡
+│ 👑 𝗦𝗬𝗦𝗧𝗘𝗠 𝗜𝗡𝗙𝗢
+│𝙾𝚂: ${os.type()} ${os.arch()}
+│𝙻𝙰𝙽𝙶 𝚅𝙴𝚁: ${process.version}
+│𝙲𝙿𝚄 𝙼𝙾𝙳𝙴𝙻: ${os.cpus()[0].model}
+│𝚂𝚃𝙾𝚁𝙰𝙶𝙴: ${usedMemoryGB.toFixed(2)} GB / ${totalMemoryGB.toFixed(2)} GB
+│𝙲𝙿𝚄 𝚄𝚂𝙰𝙶𝙴: ${cpuUsage.toFixed(1)}%
+│𝚁𝙰𝙼 𝚄𝚂𝙶𝙴: ${process.memoryUsage().heapUsed / 1024 / 1024} MB;
+├───────────────⟡
+│ ✅ 𝗢𝗧𝗛𝗘𝗥 𝗜𝗡𝗙𝗢
+│𝙳𝙰𝚃𝙴: ${date}
+│𝚃𝙸𝙼𝙴: ${time}
+│𝙿𝙸𝙽𝙶: ${ping}𝚖𝚜
+│𝚂𝚃𝙰𝚃𝚄𝚂: ${pingStatus}
+╰───────────────⟡
+`;
+
+      api.sendMessage(
+        {
+          body: systemInfo,
+        },
+        event.threadID,
+        (err, messageInfo) => {
+          if (err) {
+            console.error("Error sending message with attachment:", err);
+          } else {
+            console.log(
+              "Message with attachment sent successfully:",
+              messageInfo,
+            );
+          }
+        },
+      );
+    } catch (error) {
+      console.error("Error retrieving system information:", error);
+      api.sendMessage(
+        "Unable to retrieve system information.",
+        event.threadID,
+        event.messageID,
+      );
+    }
+  },
+};
