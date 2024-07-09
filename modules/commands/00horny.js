3558f9821461a78f6e35ad124cf49d4870d6273f
prince-kn <pe64sahani@gmail.com>
2024-07-09 16:18:26 +0530

    Update 00horny.js

--- modules/commands/00horny.js
+++ modules/commands/00horny.js
@@ -19,7 +19,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["--𝐇𝐨𝐫𝐧𝐲 𝐕𝐢𝐝𝐞𝐨 𝐁𝐲😇\n\n[» 𒄬 𓆩𓆪RKO BRO⁾𓆪』"];
+   var hi = ["--𝐇𝐨𝐫𝐧𝐲 𝐕𝐢𝐝𝐞𝐨 𝐁𝐲😇\n\n[» 𒄬 𓆪J4LVN$H1𓆪』"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
