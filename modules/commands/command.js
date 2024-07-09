2c6c355678d99d92146ad3f75de92b0258beb670
prince-kn <pe64sahani@gmail.com>
2024-07-09 22:55:48 +0530

    Update command.js

--- modules/commands/command.js
+++ modules/commands/command.js
@@ -105,7 +105,7 @@ const loadCommand = function ({ moduleList, threadID, messageID }) {
         };
     }
     if (errorList.length != 0) api.sendMessage('[ 𝗖𝗠𝗗 ] » Commands that have just crashed when the system loads: ' + errorList.join(' '), threadID, messageID);
-    api.sendMessage('[ 𝗖𝗠𝗗 ] » The Priyansh Bot system has just loaded successfully ' + (moduleList.length - errorList.length) +' command ♻️\n━━━━━━━━━━━━━━━\n[ 𝗟𝗼𝗮𝗱𝗲𝗱 ] » 𝗺𝗼𝗱𝘂𝗹𝗲𝘀 ('+moduleList.join(', ') + '.js) 💓', threadID, messageID) 
+    api.sendMessage('[ 𝗖𝗠𝗗 ] » The Karan Bot system has just loaded successfully ' + (moduleList.length - errorList.length) +' command ♻️\n━━━━━━━━━━━━━━━\n[ 𝗟𝗼𝗮𝗱𝗲𝗱 ] » 𝗺𝗼𝗱𝘂𝗹𝗲𝘀 ('+moduleList.join(', ') + '.js) 💓', threadID, messageID) 
     writeFileSync(configPath, JSON.stringify(configValue, null, 4), 'utf8')
     unlinkSync(configPath + '.temp');
     return;
