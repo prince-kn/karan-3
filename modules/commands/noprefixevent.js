18b70d74a5b4b5fbb4d946758af2afc7b305859c
prince-kn <pe64sahani@gmail.com>
2024-07-11 02:24:54 +0530

    Update noprefixevent.js

--- modules/commands/noprefixevent.js
+++ modules/commands/noprefixevent.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "\n",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "ROMIM",
+  credits: "Karan jalvanshi",
   description: "prefix event",
   commandCategory: "M H BD",
   usages: "ig",
@@ -18,9 +18,12 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["BABU KHALI PREFIX LAGANE SE CAMMAND NAHI AA JAYEGA PREFIX LAGA KE CAMMAND DE" 
+   var hi = ["«”--🤍 KARAN JALVANSHI 🤍--“»\n༆-✿ PREFIX LAGANE KE BAAD OR BHI CAMMAND DO " 
  ];
 ;
- var callback = () => api.sendMessage({body:` ${know} `,attachment: fs.createReadStream(__dirname + "/cache55.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache55.jpg"));	
+  var know = hi[Math.floor(Math.random() * hi.length)];
+  var link = [ "https://i.imgur.com/QD2iZ8y.jpeg",              
+];
+   var callback = () => api.sendMessage({body:` ${know} `,attachment: fs.createReadStream(__dirname + "/cache55.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache55.jpg"));	
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache55.jpg")).on("close",() => callback());
    };
