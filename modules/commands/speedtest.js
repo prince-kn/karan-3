c7aace57c043073046d8ae0f0e428f9bc20d0bcd
prince-kn <pe64sahani@gmail.com>
2024-07-10 15:21:10 +0530

    Update commands

---
+++ modules/commands/speedtest.js
@@ -0,0 +1,36 @@
+module.exports.config = {
+    name: "Speedtest",
+    version: "1.0.0",
+    hasPermssion: 20,
+    credits: "karan",
+    description: "Test network speed",
+    commandCategory: "system",
+    cooldowns: 15,
+    dependencies: {
+		"fast-speedtest-api": ""
+	}
+};
+
+module.exports.run = async function({ api, event }) {
+	try {
+		const fast = global.nodemodule["fast-speedtest-api"];
+		const speedTest = new fast({
+			token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm",
+			verbose: false,
+			timeout: 10000,
+			https: true,
+			urlCount: 5,
+			bufferSize: 8,
+			unit: fast.UNITS.Mbps
+		});
+		const resault = await speedTest.getSpeed();
+		return api.sendMessage(
+			"result speed test" + 
+			"\n- Speed: " + resault + " Mbps",
+			event.threadID, event.messageID
+		);
+	}
+	catch {
+		return api.sendMessage("Can't speedtest right now, try again later!", event.threadID, event.messageID);
+	}
+}
