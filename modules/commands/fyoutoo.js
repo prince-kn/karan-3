36f33d3b43da1886aa6190f098ae275eb7740f6e
prince-kn <pe64sahani@gmail.com>
2024-07-14 13:34:52 +0530

    Update fyoutoo.js

--- modules/commands/fyoutoo.js
+++ modules/commands/fyoutoo.js
@@ -15,11 +15,10 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	if (event.body.indexOf("fuck")==0 || event.body.indexOf("Fuck")==0 || event.body.indexOf("fuck you")==0 || event.body.indexOf("Fuck you")==0 || event.body.indexOf("pakyu")==0 || event.body.indexOf("Pakyu")==0 || event.body.indexOf("pak you")==0 || event.body.indexOf("Pak you")==0 || event.body.indexOf("pak u")==0 || event.body.indexOf("Pak u")==0 || event.body.indexOf("pak yu")==0 || event.body.indexOf("Pak yu")==0) {
 		var msg = {
 				body: "F you too",
-				attachment: fs.createReadStream(__dirname + `/noprefix/fuckutoo.jpeg`)
-			}
-			api.sendMessage(msg, threadID, messageID);
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
-
-  }
+			;
+  var know = hi[Math.floor(Math.random() * hi.length)];
+  var link = [ "https://i.imgur.com/LEBCNBE.jpeg",              
+];
+   var callback = () => api.sendMessage({body:` ${know} `,attachment: fs.createReadStream(__dirname + "/cache55.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache55.jpg"));	
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache55.jpg")).on("close",() => callback());
+   };
