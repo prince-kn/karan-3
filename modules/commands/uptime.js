292b666f92a977c394e5f1996b61a8720982ccb5
prince-kn <pe64sahani@gmail.com>
2024-07-16 00:45:11 +0530

    Update uptime.js

--- modules/commands/uptime.js
+++ modules/commands/uptime.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "uptime",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "karan jalvanshi",
+  credits: "Mirai-Team",
   hide: true,
   description: "Random images by api - uptime",
   commandCategory: "administrators",
@@ -47,3 +47,105 @@ var time = process.uptime(),
         `/cache/UTM-Avo.ttf`)) {
         let getfont = (await axios.get(`https://github.com/quyenkaneki/data/blob/main/UTM-Avo.ttf?raw=true`, { responseType: "arraybuffer" })).data;
         fs.writeFileSync(__dirname + `/cache/UTM-Avo.ttf`, Buffer.from(getfont, "utf-8"));
+      }
+         if (!fs.existsSync(__dirname +
+      `/cache/phenomicon.ttf`)) {
+      let getfont2 = (await axios.get(`https://github.com/quyenkaneki/data/blob/main/phenomicon.ttf?raw=true`, { responseType: "arraybuffer" })).data;
+      fs.writeFileSync(__dirname + `/cache/phenomicon.ttf`, Buffer.from(getfont2, "utf-8"));
+    };
+  if (!fs.existsSync(__dirname +
+      `/cache/CaviarDreams.ttf`)) {
+      let getfont3 = (await axios.get(`https://github.com/quyenkaneki/data/blob/main/CaviarDreams.ttf?raw=true`, { responseType: "arraybuffer" })).data;
+      fs.writeFileSync(__dirname + `/cache/CaviarDreams.ttf`, Buffer.from(getfont3, "utf-8"));
+    };
+   const { loadImage, createCanvas, registerFont } = require("canvas");
+
+  let k = args[0];
+   if(args[0] == "list"){
+    const alime = (await axios.get('https://raw.githubusercontent.com/quyenkaneki/data/main/dataanime.json')).data
+    var count = alime.listAnime.length;
+      var data = alime.listAnime
+      var page = 1;
+      page = parseInt(args[1]) || 1;
+      page < -1 ? page = 1 : "";
+      var limit = 20;
+      var numPage = Math.ceil(count/limit);
+      var msg = ``;
+      for(var i = limit*(page - 1); i < limit*(page-1) + limit; i++){
+         if(i >= count) break;
+        msg += `[ ${i+1} ] - ${data[i].ID} | ${data[i].name}\n`;
+      }
+      msg += `Trang (${page}/${numPage})\nDùng ${global.config.PREFIX}${this.config.name} list <số trang>`;
+      return api.sendMessage(msg, event.threadID,event.messageID);
+   }
+  if(!k){
+  var id = Math.floor(Math.random() * 848) +1
+  } else {
+    var id = k
+  }
+    const lengthchar = (await axios.get('https://raw.githubusercontent.com/quyenkaneki/data/main/dataimganime.json')).data
+    console.log(lengthchar.length)
+    const Canvas = require('canvas');
+    let pathImg = __dirname + `/cache/${id}.png`;
+    let pathAva = __dirname + `/cache/${event.senderID}.png`;
+    let background = (await axios.get(encodeURI(`https://imgur.com/x5JpRYu.png`), { responseType: "arraybuffer" })).data;
+    fs.writeFileSync(pathImg, Buffer.from(background, "utf-8"));
+    let ava = (await axios.get(encodeURI(`${lengthchar[id].imgAnime}`), { responseType: "arraybuffer" })).data;
+    fs.writeFileSync(pathAva, Buffer.from(ava, "utf-8"));
+    const request = require('request');
+    const path = require('path');
+
+  //const a = Math.floor(Math.random() * 820) + 1
+
+
+let l1 = await loadImage(pathAva);
+    let a = await loadImage(pathImg);
+    let canvas = createCanvas(a.width, a.height);
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = lengthchar[id].colorBg;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(l1, -200, -200, 1200, 1200);
+    ctx.drawImage(a, 0, 0, canvas.width, canvas.height);
+     registerFont(__dirname + `/cache/phenomicon.ttf`, {
+      family: "phenomicon"
+    });
+    ctx.textAlign = "start";
+    ctx.strokeStyle = lengthchar[id].colorBg;
+    ctx.filter = "brightness(90%) contrast(110%)";
+    ctx.font = "130px phenomicon";
+    ctx.fillStyle = lengthchar[id].colorBg;
+    ctx.fillText(global.config.BOTNAME, 835, 340);
+    ctx.beginPath();
+  ////////////////////////////////////////
+   registerFont(__dirname + `/cache/UTM-Avo.ttf`, {
+      family: "UTM"
+    });
+    ctx.textAlign = "start";
+    ctx.font = "70px UTM";
+    ctx.fillStyle = "#000000";
+    ctx.fillText(`${z_1} : ${x_1} : ${y_1} `, 980, 440);
+    ctx.restore();
+    ctx.save();
+registerFont(__dirname + `/cache/CaviarDreams.ttf`, {
+      family: "time"
+    });
+    ctx.textAlign = "start";
+    ctx.font = "55px time";
+    ctx.fillText("EREN", 930, 540)
+    ctx.fillText("Eren Yeager メ", 930, 610)
+    ctx.fillText("Eren Yeager メ", 930, 690)
+    ctx.restore();
+    ctx.save();
+    ctx.beginPath();
+    const imageBuffer = canvas.toBuffer();
+   fs.writeFileSync(pathImg, imageBuffer);
+  return api.sendMessage({
+    body: `Server Information\n\nChip: ${chips}.\nProcessing Speed: ${speed}MHz.\n\nTotal Memory: ${byte2mb(os.totalmem())}.\nUsed: ${byte2mb(os.freemem())} (${(os.freemem() * 100 / os.totalmem()).toFixed()}%).\n\nBot is running up ${hours} hours ${minutes} minute ${seconds} seconds.\n\n❯ Total users: ${global.data.allUserID.length}\n❯ Total Group: ${global.data.allThreadID.length}\n❯ CPU in use: ${pidusage.cpu.toFixed(1)}%\n❯ Ram in use: ${byte2mb(pidusage.memory)}\n❯ Ping: ${Date.now() - timeStart}ms\n❯ Character ID: ${id}\n❯ Admin Faceboook:- https://www.facebook.com/Legend.king.jalvanshi`,
+    attachment: fs.createReadStream(pathImg)
+  },
+    event.threadID,
+    () => fs.unlinkSync(pathImg),
+    fs.unlinkSync(pathAva),
+    event.messageID
+  );
+}
